29de88d3de684df0a1f02ef829d63e7ee915bd13
Saddambmas86@gmail.com <Saddambmas86@gmail.com>
2026-03-28 23:48:54 +0530

    Update FB link

--- includes/sharer.php
+++ includes/sharer.php
@@ -1,14 +1,20 @@
 <center>
-<!-- SHARER -->
-<div class="sharer">
-  <div class="share-text"><i class="fa fa-share-alt" aria-hidden="true"></i> Share:</div>
-  <a class="share-button facebook" href="https://www.facebook.com/sharer/sharer.php?u={{url}}" target="_blank"><i class="fab fa-facebook-f"></i></a>
-  <a class="share-button twitter" href="https://twitter.com/intent/tweet?url={{url}}&text={{title}}" target="_blank"><i class="fab fa-twitter"></i></a>
-  <a class="share-button reddit" href="https://www.reddit.com/submit?url={{url}}&title={{title}}" target="_blank"><i class="fab fa-reddit"></i></a>
-  <a class="share-button whatsapp" href="whatsapp://send?text={{title}} - {{url}}" target="_blank"><i class="fab fa-whatsapp"></i></a>
-  <a class="share-button telegram" href="https://telegram.me/share/url?url={{url}}&text={{title}}" target="_blank"><i class="fab fa-telegram"></i></a>
-  <a class="share-button copy-url" href="javascript:void(0);" onclick="copyURL(event)" title="Copy URL"><i class="fa fa-link"></i></a>
-</div>
-<script src="assets/js/share.min.js"></script>
-<!-- SHARER -->
+  <!-- SHARER -->
+  <div class="sharer">
+    <div class="share-text"><i class="fa fa-share-alt" aria-hidden="true"></i> Share:</div>
+    <a class="share-button facebook" href="https://www.facebook.com/profile.php?id=61552394730687" target="_blank"><i
+        class="fab fa-facebook-f"></i></a>
+    <a class="share-button twitter" href="https://twitter.com/intent/tweet?url={{url}}&text={{title}}"
+      target="_blank"><i class="fab fa-twitter"></i></a>
+    <a class="share-button reddit" href="https://www.reddit.com/submit?url={{url}}&title={{title}}" target="_blank"><i
+        class="fab fa-reddit"></i></a>
+    <a class="share-button whatsapp" href="whatsapp://send?text={{title}} - {{url}}" target="_blank"><i
+        class="fab fa-whatsapp"></i></a>
+    <a class="share-button telegram" href="https://telegram.me/share/url?url={{url}}&text={{title}}" target="_blank"><i
+        class="fab fa-telegram"></i></a>
+    <a class="share-button copy-url" href="javascript:void(0);" onclick="copyURL(event)" title="Copy URL"><i
+        class="fa fa-link"></i></a>
+  </div>
+  <script src="assets/js/share.min.js"></script>
+  <!-- SHARER -->
 </center>
